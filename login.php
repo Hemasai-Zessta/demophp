d768727b6c829e092f967c0d400a1ad3e686a391
Pavan <pavan.kuracha@zessta.com>
2023-01-31 16:27:58 +0530

    login.php is added

---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html >
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <div>
+        <form action="login_authentication.php" method="POST">
+            <div>
+                <br/><br/><br/><br/>
+            </div>	
+				<center>
+                    <h2>Login here</h2>
+                    <div>
+                        <label><b>Username :</b></label>
+                        <input type="text" name="username" />
+                    </div>
+                    <div>
+                        <label><b>Password :</b></label>
+                        <input type="password" name="password" />
+                    </div>
+                    <br />
+                    <div >
+                        <button name="login">Login</button>
+                    </div>
+                    <a href="register.php">Registration</a>
+                </center>
+			</form>
+    </div>
+</body>
+</html>
